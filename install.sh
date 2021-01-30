383735c6d4c78ab098b08b8ff580696daa8b93fb
DeadShot0x7 <68690437+Deadshot0x7@users.noreply.github.com>
2021-01-31 00:55:26 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo Connecting to the python index manager
+echo Started Downloading 
+sudo apt install python3-pip
+echo pip Installed 
+sudo apt insatll phonenumbers
+echo PhoneNumberInofrmation Database Downloaded 
+sudo apt install google
+echo Google Installed
+sudo apt install pyfiglet 
+echo Important libaries downloaded Sucessfuly 
