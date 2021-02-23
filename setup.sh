78cafc25a1aa9bcd39bd21111f477c304f56871b
DeadShot0x7 <68690437+Deadshot0x7@users.noreply.github.com>
2021-02-23 15:43:36 +0530

    Update setup.sh
    fixed the setup. sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 echo Connecting to the python index manager
-echo Started Downloading 
- apt install python3-pip
 echo pip Installed 
- apt insatll phonenumbers
+ pip insatll phonenumbers
 echo PhoneNumberInofrmation Database Downloaded 
- apt install google
+ pip install google
 echo Google Installed
- apt install pyfiglet 
+ pip install pyfiglet 
 echo Important libaries downloaded Sucessfuly 
